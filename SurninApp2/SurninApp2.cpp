9e6e1332a80c2194a9e890e0aadf910d76040cf8
Mikhail_Surnin <mikesurnin1996@gmail.com>
2019-03-18 22:25:46 +0500

    Добавьте файлы проекта.

---
+++ SurninApp2/SurninApp2.cpp
@@ -0,0 +1,22 @@
+﻿#include <windows.h>
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+typedef int(*TEST)();
+int main()
+{
+	HINSTANCE hDll = LoadLibrary(L"SurninDynDLL");
+	TEST pFIO = NULL;
+	TEST pGroup = NULL;
+	pFIO = (TEST)GetProcAddress(hDll, "FIO");
+	pGroup = (TEST)GetProcAddress(hDll, "Group");
+	pFIO();
+	pGroup();
+	FreeLibrary(hDll);
+	system("pause");
+	return 0;
+	
+}
+
+
